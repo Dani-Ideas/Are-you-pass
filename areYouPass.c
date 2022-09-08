111fb8d997daae398825ff7b55c3af36df0a569d
Dani-Ideas <d7n456.17@gmail.com>
2022-09-08 11:11:53 -0500

    the code use if and I was clar the bugs

--- areYouPass.c
+++ areYouPass.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
-
+int note;
 
 int main ()
 {
-    int note = 12;
+	note=11;
+	printf("Hello student would you like know your note?\n");
     if (note > 0 && note<11)
         {
-        if (note == 10)  
-	    {
-	    	printf("Ok you are on fire");
+			printf("Great your note is %d \n", note);
+        	if (note == 10)  
+	    	{
+	    		printf("Ok you are on fire");
+	    	}
+        	else if (note>8|| note==8)// I know was could use note>7, but I wnato check the operator OR  
+	    	{
+	    		printf("Great job you are succes the subjet");
+	    	}
+	    	else if (note>6)
+	    	{
+	    		printf("Nice try your note is very well, but I know you can do better");
+	    	}
+	    	else if (note==6)
+	    	{
+	    		printf("Uff it was too close, but you are save");
+	    	}
+			else
+	    	{
+	    		printf("Sorry my friend your note isn't such for pass the subject \n Should you check when is the next enroll");
+	    	}
 	    }
-        else if (note > 9)  
-	    {
-	    	printf("Great job you are succes the subjet");
-	    }
-	    else if (note>7)
-	    {
-	    	printf("Nice try your note is very well, but I know you can do better");
-	    }
-	    else if (7>note)
-	    {
-	    	printf("Uff it was too close, but you are save");
-	    }
-        else if (note>6)
-	    {
-	    	printf("Uff it was too close, but you are save");
-	    }
-	    else
-	    {
-	    	printf("Sorry my friend your note isn't such for pass the subject \n Should you check when is the next enroll");
-	    }
-        }
-	    else
-        printf("Are you kidding me?");
+	else
+	{
+		printf("Are you kidding me? It's imposible to get this note");
+	}
 }
